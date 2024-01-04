6533b20fe6eecfdaa458e1632c2431631d3def62
NikolayDPaev <niki.paev@gmail.com>
2024-01-04 18:33:24 +0200

    new comments list

--- clasiffy_comments.py
+++ clasiffy_comments.py
@@ -4,31 +4,31 @@ import msvcrt
 
 def load_comments():
     comments = []
-    with open('data/blitz_comments_classified.json', 'r', encoding="utf-8") as f:
+    with open('data/blitz_comments.json', 'r', encoding="utf-8") as f:
         comments = json.load(f)
     return comments
 
 def save_comments(comments):
     json_object = json.dumps(list(comments), indent=4, ensure_ascii=False)
-    with codecs.open("data/blitz_comments_classified.json", "w", "utf-8") as outfile:
+    with codecs.open("data/blitz_comments.json", "w", "utf-8") as outfile:
         outfile.write(json_object)
 
 def main():
     comments = load_comments()
     for comment in comments:
-        if len(comment) == 3:
+        if 'label' in comment:
             continue
-        print(comment[0])
+        print(comment['comment'])
         ch: str = msvcrt.getch().decode("utf-8")
         if ch == 'p':
-            comment.append('p')
+            comment['label'] = 'p'
         if ch == 'n':
-            comment.append('n')
+            comment['label'] = 'n'
         if ch == 'c':
-            comment.clear()
+            comment['clear'] = True
         if ch == 'e':
             break
-    save_comments([c for c in comments if len(c) > 0])
+    save_comments([c for c in comments if 'clear' not in c])
 
 if __name__ == "__main__":
     main()
